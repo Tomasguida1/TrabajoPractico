45374a636cd52656a1dd21e6829bd87a4ade3929
thedraxx <55867157+thedraxx@users.noreply.github.com>
2021-09-01 13:59:47 -0300

    Segundo Commit

--- TrabajoPractico.php
+++ TrabajoPractico.php
@@ -1,1 +1,9 @@
-<!-- Un bucle for que indica inicio, mitad y final del recorrido -->
+<?php
+$o = 10;
+for ($i = 0; $i > $o; $i++) {
+    if ($i < 10) {
+        echo "Todabia no supero el numero 10";
+    } else {
+        echo "supero el numero 10";
+    }
+}
