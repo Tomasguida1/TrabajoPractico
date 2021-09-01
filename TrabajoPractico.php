3bed3a1fe70a1b13e7357bc3dc0d2c2a7446527c
thedraxx <55867157+thedraxx@users.noreply.github.com>
2021-09-01 14:00:20 -0300

    Tercer Commit

--- TrabajoPractico.php
+++ TrabajoPractico.php
@@ -3,6 +3,8 @@ $o = 10;
 for ($i = 0; $i > $o; $i++) {
     if ($i < 10) {
         echo "Todabia no supero el numero 10";
+    } elseif ($i === 5) {
+        echo "mitad de camino...";
     } else {
         echo "supero el numero 10";
     }
